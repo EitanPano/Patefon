3cfa5eb236de4544928eb092f400decf7a49d9f4
Nir Funt <Antiflag77@gmail.com>
2021-12-04 13:22:33 +0200

    fixed set to array

--- src/views/search.vue
+++ src/views/search.vue
@@ -47,7 +47,7 @@ export default {
         type: "songToPlayer",
         song,
         idx,
-        station: { songs: JSON.parse(JSON.stringify(this.songs)) },
+        station: { songs: JSON.parse(JSON.stringify(Array.from(this.songs))) },
       });
     },
   },

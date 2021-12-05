4db9881c58d2bd4d8929524aa9c8852fea6e96da
Mark Fishman <gibor1122@gmail.com>
2021-12-05 16:31:11 +0200

    genre cards and see more updated

--- src/views/genre.vue
+++ src/views/genre.vue
@@ -1,32 +1,37 @@
-<template>
- <section class="main-layout genre">
-     <h1> {{genre}} </h1>
- <station-list v-if="genre" :stations="stationsByGenre[genre]" />
-</section>
+ <template>
+  <section class="main-layout">
+    <h1 class="full highlight">{{ genre }}</h1>
+    <div class="grid-container">
+      <template v-for="station in stationsByGenre[genre]">
+        <station-preview :station="station" :key="station._id" />
+      </template>
+    </div>
+  </section>
 </template>
 
 <script>
 import stationList from "../components/station-list.vue";
+import stationPreview from "../components/station-preview.vue";
 export default {
-     components: {
+  components: {
     stationList,
+    stationPreview,
   },
-data () {
+  data() {
     return {
-        genre : '',
-    }
-},
-created() {
-   this.genre = this.$route.params.id;
-},
-computed : {
-  stationsByGenre() {
+      genre: "",
+    };
+  },
+  created() {
+    this.genre = this.$route.params.id;
+  },
+  computed: {
+    stationsByGenre() {
       return this.$store.getters.stationsByGenre;
     },
-},
-}
+  },
+};
 </script>
 
 <style>
-
 </style>

955e1e096a4821b1be220787be393218c0efbdde
Eitan Pano <eitanpano95@gmail.com>
2021-12-05 18:27:05 +0200

    added responsiveness

--- src/components/station-list.vue
+++ src/components/station-list.vue
@@ -1,55 +1,55 @@
 <template>
-  <ul class="station-list" v-if="stations && stations.length">
-    <!-- <h1> {{ widthOutput }}</h1> -->
-    <li v-for="station in fixedStations" :key="station._id">
-      <station-preview :station="station" @remove="remove" @update="update" />
-    </li>
-  </ul>
+    <ul class="station-list" v-if="stations && stations.length">
+        <li v-for="station in fixedStations" :key="station._id">
+            <station-preview :station="station" @remove="remove" @update="update" />
+        </li>
+    </ul>
 </template>
 
 <script>
 import stationPreview from "./station-preview.vue";
 export default {
-  components: {
-    stationPreview,
-  },
-  props: ["stations"],
-  data() {
-    return {
-      widthOutput: null,
-    };
-  },
+    components: {
+        stationPreview,
+    },
+    props: ["stations"],
+    data() {
+        return {
+            widthOutput: null,
+        };
+    },
 
-  created() {
-    this.widthOutput = window.innerWidth;
-    window.addEventListener("resize", () => {
-      this.widthOutput = window.innerWidth;
-      console.log(this.widthOutput);
-    });
-    // window.onresize = reportWindowSize;
-  },
+    created() {
+        this.widthOutput = window.innerWidth;
+        window.addEventListener("resize", () => {
+            this.widthOutput = window.innerWidth;
+        });
+    },
 
-  methods: {
-    remove(stationId) {
-      this.$emit("remove", stationId);
+    methods: {
+        remove(stationId) {
+            this.$emit("remove", stationId);
+        },
+        update(station) {
+            this.$emit("update", station);
+        },
     },
-    update(station) {
-      this.$emit("update", station);
+    computed: {
+        fixedStations() {
+            if (this.widthOutput <= 600) return this.stations.slice(0, 2);
+            else if (this.widthOutput <= 850) return this.stations.slice(0, 3);
+            else if (this.widthOutput <= 1050) return this.stations.slice(0, 3);
+            else if (this.widthOutput <= 1250) return this.stations.slice(0, 4);
+            else if (this.widthOutput <= 1450) return this.stations.slice(0, 5);
+            else if (this.widthOutput <= 1650) return this.stations.slice(0, 6);
+            else if (this.widthOutput <= 1850) return this.stations.slice(0, 7);
+            else if (this.widthOutput <= 2050) return this.stations.slice(0, 8);
+            else if (this.widthOutput <= 2250) return this.stations.slice(0, 9);
+            else if (this.widthOutput <= 2450)
+                return this.stations.slice(0, 10);
+            // else if (this.widthOutput >= 1000) return this.stations.slice(0, 4)
+        },
     },
-  },
-  computed: {
-    fixedStations() {
-      if (this.widthOutput <= 1050) return this.stations.slice(0, 3);
-      else if (this.widthOutput <= 1250) return this.stations.slice(0, 4);
-      else if (this.widthOutput <= 1450) return this.stations.slice(0, 5);
-      else if (this.widthOutput <= 1650) return this.stations.slice(0, 6);
-      else if (this.widthOutput <= 1850) return this.stations.slice(0, 7);
-      else if (this.widthOutput <= 2050) return this.stations.slice(0, 8);
-      else if (this.widthOutput <= 2250) return this.stations.slice(0, 9);
-      else if (this.widthOutput <= 2450) return this.stations.slice(0, 10);
-      // else if (this.widthOutput >= 1000) return this.stations.slice(0, 4)
-    },
-  },
 };
 </script>
 

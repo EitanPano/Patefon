52a2ba84f021c11609a77c5b98ed507588a21883
Eitan Pano <eitanpano95@gmail.com>
2021-12-09 19:40:43 +0200

    added styles

--- src/components/station-preview.vue
+++ src/components/station-preview.vue
@@ -1,36 +1,36 @@
 <template>
-  <article
-    @mouseleave="isHover = false"
-    @mouseenter="isHover = true"
-    @click="enterStation(station._id)"
-    class="station-preview"
-  >
-    <div>
-      <img :src="station.imgUrl" />
-    </div>
-    <p>{{ station.name }}</p>
-    <p>By {{ station.createdBy.name }}</p>
-    <transition name="fade">
-      <button class="btn btn-play" v-if="isHover">▶</button>
-    </transition>
-  </article>
+    <article
+        @mouseleave="isHover = false"
+        @mouseenter="isHover = true"
+        @click="enterStation(station._id)"
+        class="station-preview"
+    >
+        <div>
+            <img :src="station.imgUrl" />
+            <transition name="fade">
+                <button class="btn btn-play" v-if="isHover"><span class="material-icons">play_arrow</span></button>
+            </transition>
+        </div>
+        <p>{{ station.name }}</p>
+        <p>By {{ station.createdBy.name }}</p>
+    </article>
 </template>
 
 <script>
 export default {
-  props: ["station"],
-  data() {
-    return {
-      isHover: false,
-    };
-  },
-  created() {},
-  methods: {
-    enterStation(stationId) {
-      this.$router.push("/station/" + stationId);
+    props: ["station"],
+    data() {
+        return {
+            isHover: false,
+        };
     },
-  },
-  computed: {},
+    created() {},
+    methods: {
+        enterStation(stationId) {
+            this.$router.push("/station/" + stationId);
+        },
+    },
+    computed: {},
 };
 </script>
 
--- src/views/station.vue
+++ src/views/station.vue
@@ -1,28 +1,31 @@
 <template>
-  <section class="main-layout station">
-    <main>
-      <img class="station-img" :src="this.currStation.imgUrl" alt="" />
-      <div class="flex column">
-        <p class="highlight small">PLAYLIST</p>
-        <h1>{{ currStation.name }}</h1>
-        <div>
-          <span class="user-icon material-icons">account_circle</span>
-          <p class="line-h-0 small" v-if="songsCount && songsCount.length">
-            <span class="highlight">Guest</span> • {{ songsCount.length }} Songs
-          </p>
-        </div>
-      </div>
-    </main>
-      <chat-room :currStation="currStation" v-if="currStation" />
-    <song-list
-      :songs="currStation.songs"
-      @removeSong="removeSong"
-      @songToPlayer="songToPlayer"
-      @swapped="swapIdxs"
-      @likeSong="updateUser"
-    />
-
-  </section>
+    <section :class="bgColor" class="main-layout station">
+        <main>
+            <img class="station-img" :src="this.currStation.imgUrl" alt="" />
+            <div class="flex column">
+                <p class="highlight small">PLAYLIST</p>
+                <h1>{{ currStation.name }}</h1>
+                <div>
+                    <span class="user-icon material-icons">account_circle</span>
+                    <p
+                        class="line-h-0 small"
+                        v-if="songsCount && songsCount.length"
+                    >
+                        <span class="highlight">Guest</span> •
+                        {{ songsCount.length }} Songs
+                    </p>
+                </div>
+            </div>
+        </main>
+        <chat-room :currStation="currStation" v-if="currStation" />
+        <song-list
+            :songs="currStation.songs"
+            @removeSong="removeSong"
+            @songToPlayer="songToPlayer"
+            @swapped="swapIdxs"
+            @likeSong="updateUser"
+        />
+    </section>
 </template>
 
 <script>
@@ -31,101 +34,115 @@ import chatRoom from "../components/chat-room.vue";
 import shareListen from "../components/share-listen.vue";
 import { socketService } from "../services/socket.service";
 export default {
-  components: {
-    songList,
-    chatRoom,
-    shareListen,
-  },
-  data() {
-    return {
-      isLikedStation: null,
-    };
-  },
+    components: {
+        songList,
+        chatRoom,
+        shareListen,
+    },
+    data() {
+        return {
+            isLikedStation: null,
+            gradients: [
+                "grad-red",
+                "grad-sky",
+                "grad-mint",
+                "grad-orange",
+                "grad-blue",
+                "grad-pink",
+                "grad-purple",
+                "grad-green",
+            ],
+        };
+    },
 
-  methods: {
-    async swapIdxs(moved) {
-      try {
-        const newStation = JSON.parse(JSON.stringify(this.currStation));
-        newStation.songs.splice(moved.oldIndex, 1);
-        newStation.songs.splice(moved.newIndex, 0, moved.element);
-        // console.log('newStation', newStation);
-        await this.$store.dispatch({
-          type: "updateStation",
-          station: newStation,
-        });
-      } catch (err) {
-        console.log(err);
-      }
+    methods: {
+        async swapIdxs(moved) {
+            try {
+                const newStation = JSON.parse(JSON.stringify(this.currStation));
+                newStation.songs.splice(moved.oldIndex, 1);
+                newStation.songs.splice(moved.newIndex, 0, moved.element);
+                // console.log('newStation', newStation);
+                await this.$store.dispatch({
+                    type: "updateStation",
+                    station: newStation,
+                });
+            } catch (err) {
+                console.log(err);
+            }
+        },
+        async removeSong(songId) {
+            try {
+                const idx = this.currStation.songs.findIndex(
+                    (song) => song.id === songId
+                );
+                this.currStation.songs.splice(idx, 1);
+                await this.$store.dispatch({
+                    type: "updateStation",
+                    station: this.currStation,
+                });
+            } catch (err) {
+                console.log(err);
+            }
+        },
+        songToPlayer(song, idx) {
+            this.$store.commit({
+                type: "songToPlayer",
+                song,
+                idx,
+                station: this.currStation,
+            });
+        },
+        updateUser(action) {
+            console.log("updating");
+            this.$store.dispatch({
+                type: "updateUser",
+                action,
+            });
+        },
+        goToSearch() {
+            console.log("searching...");
+            this.$router.push(`/search`);
+        },
+        destroyed() {
+            this.$store.dispatch({
+                type: "updateStation",
+                station: this.currStation,
+            });
+            this.$store.dispatch({
+                type: "setFilter",
+                filterBy: {},
+            });
+            socketService.off("get share-listen");
+        },
     },
-    async removeSong(songId) {
-      try {
-        const idx = this.currStation.songs.findIndex(
-          (song) => song.id === songId
-        );
-        this.currStation.songs.splice(idx, 1);
-        await this.$store.dispatch({
-          type: "updateStation",
-          station: this.currStation,
-        });
-      } catch (err) {
-        console.log(err);
-      }
+    computed: {
+        currStation() {
+            return this.$store.getters.currStation;
+        },
+        stationName() {
+            return this.currStation.name;
+        },
+        songsCount() {
+            return this.currStation.songs;
+        },
+        bgColor() {
+            const idx = Math.floor(Math.random() * this.gradients.length);
+            return this.gradients[idx];
+        },
     },
-    songToPlayer(song, idx) {
-      this.$store.commit({
-        type: "songToPlayer",
-        song,
-        idx,
-        station: this.currStation,
-      });
+    watch: {
+        "$route.params.id": {
+            async handler() {
+                try {
+                    const id = this.$route.params.id;
+                    await this.$store.dispatch({ type: "getById", id });
+                } catch (err) {
+                    console.log(err);
+                }
+            },
+            immediate: true,
+        },
     },
-    updateUser(action) {
-      console.log("updating");
-      this.$store.dispatch({
-        type: "updateUser",
-        action,
-      });
-    },
-    goToSearch() {
-      console.log("searching...");
-      this.$router.push(`/search`);
-    },
-    destroyed() {
-      this.$store.dispatch({
-        type: "updateStation",
-        station: this.currStation,
-      });
-      this.$store.dispatch({
-        type: "setFilter",
-        filterBy: {},
-      });
-      socketService.off("get share-listen");
-    },
-  },
-  computed: {
-    currStation() {
-      return this.$store.getters.currStation;
-    },
-    stationName() {
-      return this.currStation.name;
-    },
-    songsCount() {
-      return this.currStation.songs;
-    },
-  },
-  watch: {
-    "$route.params.id": {
-      async handler() {
-        try {
-          const id = this.$route.params.id;
-          await this.$store.dispatch({ type: "getById", id });
-        } catch (err) {
-          console.log(err);
-        }
-      },
-      immediate: true,
-    },
-  },
 };
 </script>
 

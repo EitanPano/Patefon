d442638bbaa8786f5abbef6668016260a13e5fab
Eitan Pano <eitanpano95@gmail.com>
2021-12-09 22:14:23 +0200

    added welcome page

---
+++ src/components/landing.vue
@@ -0,0 +1,25 @@
+<template>
+    <transition name="fade">
+        <section class="landing">
+            <h1>Patefon</h1>
+            <h2>It's All About The Music</h2>
+            <img src="img/logo/patefon.png" alt="patefon" />
+            <p>Welcome to Patefon</p>
+            <button @click="$emit('explore')">START THE BEAT</button>
+
+            <div class="bubble s1 small"></div>
+            <div class="bubble s2 small"></div>
+            <div class="bubble s3 small"></div>
+            <div class="bubble s4 small"></div>
+            <div class="bubble m1 medium"></div>
+            <div class="bubble b1 big"></div>
+        </section>
+    </transition>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
